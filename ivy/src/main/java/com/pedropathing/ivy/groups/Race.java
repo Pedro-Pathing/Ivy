bfd6e0cafdcb63ca243d5068b9f51d1fbdc6d236
Hypothetical <adi.varshney09@gmail.com>
2025-12-08 00:50:24 -0500

    Add Race command group implementation
    Introduces the Race class implementing ICommand, allowing multiple commands to run in parallel and marking completion when any command finishes. Includes methods for execution, requirements, interruptibility, copying, starting, and ending commands.

---
+++ ivy/src/main/java/com/pedropathing/ivy/groups/Race.java
@@ -0,0 +1,75 @@
+package com.pedropathing.ivy.groups;
+
+import com.pedropathing.ivy.ICommand;
+import com.pedropathing.ivy.Interruptibility;
+
+import java.util.*;
+
+public class Race implements ICommand {
+    protected boolean raceCompleted = false;
+    public Race(ICommand... cmds) {
+        commands.addAll(Arrays.asList(cmds));
+
+//        HashSet<Object> commandSet = new HashSet<>();
+//        for (ICommand command : commands) {
+//            commandSet.addAll(command.getRequirements());
+//        }
+//        requirements = new ArrayList<>(commandSet);
+    }
+
+    @Override
+    public void execute() {
+        if (!done()) {
+            Iterator<ICommand> it = commands.iterator();
+            while (it.hasNext()) {
+                ICommand command = it.next();
+                if (command.done()) {
+                    raceCompleted = true;
+                    break;
+                } else {
+                    command.execute();
+                }
+            }
+        }
+    }
+
+    @Override
+    public List<Object> getRequirements() {
+        return requirements;
+    }
+
+    @Override
+    public Interruptibility getInterruptibility() {
+        return Interruptibility.UNINTERRUPTIBLE;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        for (ICommand command : commands) {
+            command.end(interrupted);
+        }
+        commands.clear();
+    }
+
+    @Override
+    public ICommand copy() {
+        ICommand[] cmds = new ICommand[commands.size()];
+        int i = 0;
+        for (ICommand command : commands) {
+            cmds[i++] = command.copy();
+        }
+        return new Race(cmds);
+    }
+
+    @Override
+    public void start() {
+        for (ICommand command : commands) {
+            command.start();
+        }
+    }
+
+    @Override
+    public boolean done() {
+        return raceCompleted;
+    }
+}
