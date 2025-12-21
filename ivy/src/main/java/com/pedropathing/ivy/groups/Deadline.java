aec0a0413904166ff9dc0d9a9da17e46c9674220
Kabir Goyal <kabirgoyal@icloud.com>
2025-12-21 17:50:17 -0500

    Added dealine command groups

---
+++ ivy/src/main/java/com/pedropathing/ivy/groups/Deadline.java
@@ -0,0 +1,106 @@
+package com.pedropathing.ivy.groups;
+
+import com.pedropathing.ivy.ICommand;
+import com.pedropathing.ivy.Interruptibility;
+
+import java.util.*;
+
+public class Deadline implements ICommand {
+  private LinkedList<ICommand> commands = new LinkedList<>();
+  private ICommand deadlineCommand;
+  private List<Object> requirements = new ArrayList<>();
+  private boolean done;
+
+  public Deadline(ICommand... cmds) {
+    deadlineCommand = cmds[0];
+    commands.addAll(Arrays.asList(cmds).subList(1, cmds.length));
+    rebuildRequirements();
+  }
+
+  @Override
+  public void execute() {
+    if (!done()) {
+      if (deadlineCommand.done()) {
+        done = true;
+        deadlineCommand.end(false);
+        for (ICommand command : commands) {
+          command.end(true);
+        }
+        commands.clear();
+        return;
+      }
+      deadlineCommand.execute();
+
+      Iterator<ICommand> it = commands.iterator();
+      while (it.hasNext()) {
+        ICommand command = it.next();
+        if (command.done()) {
+          command.end(false);
+          it.remove();
+        } else {
+          command.execute();
+        }
+      }
+    }
+  }
+
+  @Override
+  public List<Object> getRequirements() {
+    return requirements;
+  }
+
+  @Override
+  public Interruptibility getInterruptibility() {
+    return Interruptibility.UNINTERRUPTIBLE;
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    for (ICommand command : commands) {
+      command.end(interrupted);
+    }
+
+    if (!done) {
+      deadlineCommand.end(interrupted);
+    }
+
+    commands.clear();
+    done = true;
+  }
+
+  @Override
+  public ICommand copy() {
+    ICommand[] cmds = new ICommand[commands.size()];
+    int i = 0;
+    for (ICommand command : commands) {
+      cmds[i++] = command.copy();
+    }
+    return new Deadline(deadlineCommand.copy(), cmds);
+  }
+
+  @Override
+  public void start() {
+    deadlineCommand.start();
+    for (ICommand command : commands) {
+      command.start();
+    }
+  }
+
+  @Override
+  public boolean done() {
+    return done;
+  }
+
+  private void rebuildRequirements() {
+    Set<Object> set = new HashSet<>();
+    List<Object> deadlineRequirements = deadlineCommand.getRequirements();
+    if (deadlineRequirements != null)
+      set.addAll(deadlineRequirements);
+    for (ICommand command : commands) {
+      List<Object> r = command.getRequirements();
+      if (r != null)
+        set.addAll(r);
+    }
+    requirements = new ArrayList<>(set);
+  }
+}
