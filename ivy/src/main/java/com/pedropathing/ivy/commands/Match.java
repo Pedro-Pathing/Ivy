b70003f11075359818485feb3741837e4fa3c118
Kabir Goyal <kabirgoyal@icloud.com>
2025-12-26 15:10:03 -0500

    Match command

---
+++ ivy/src/main/java/com/pedropathing/ivy/commands/Match.java
@@ -0,0 +1,46 @@
+package com.pedropathing.ivy.commands;
+
+import com.pedropathing.ivy.Command;
+import com.pedropathing.ivy.ICommand;
+
+import java.util.EnumMap;
+import java.util.function.Supplier;
+
+public class Match<T extends Enum<T>> extends Command {
+    private Supplier<T> stateSupplier;
+    private T state;
+    private EnumMap<T, ICommand> cases;
+
+    public Match(Supplier<T> stateSupplier, EnumMap<T, ICommand> cases) {
+        this.stateSupplier = stateSupplier;
+        this.cases = cases;
+    }
+
+    @Override
+    public void start() {
+        state = stateSupplier.get();
+        if (cases.get(state) != null) {
+            adoptBehavior(cases.get(state));
+        } else {
+            setDone(() -> true);
+        }
+    }
+
+    private void adoptBehavior(ICommand v) {
+        v.start();
+        setExecute(v::execute);
+        setEnd(v::end);
+        setDone(v::done);
+        setInterruptibility(v.getInterruptibility());
+        setChainability(v.getChainability());
+        if (v.getRequirements() != null)
+            setRequirements(v.getRequirements().toArray());
+    }
+
+    @Override
+    public Match<T> copy() {
+        EnumMap<T, ICommand> copiedCases = cases.clone();
+        copiedCases.replaceAll((k, v) -> v.copy());
+        return new Match<>(stateSupplier, copiedCases);
+    }
+}
