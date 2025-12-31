55369a3ce1b750405e8109476daf8d29cf017203
Kabir <kabirgoyal@icloud.com>
2025-12-30 23:23:31 -0500

    Infinite impl

--- ivy/src/main/java/com/pedropathing/ivy/commands/Infinite.java
+++ ivy/src/main/java/com/pedropathing/ivy/commands/Infinite.java
@@ -1,21 +1,23 @@
 package com.pedropathing.ivy.commands;
 
+import com.pedropathing.ivy.CommandBuilder;
+
 /**
  * A command that runs indefinitely until interrupted.
- * 
+ *
  * @version 1.0
  * @author Baron Henderson
  * @author Kabir Goyal
  */
-public class Infinite extends CommandClass {
+public class Infinite extends CommandBuilder {
     /**
      * Constructs a new Infinite command that runs the given runnable
      * indefinitely until interrupted.
-     * 
+     *
      * @param r the runnable (void function with no parameters) to execute
      */
     public Infinite(Runnable r) {
-        super.setExecute(r);
-        super.setDone(() -> false);
+        setExecute(r);
+        setDone(() -> false);
     }
 }
