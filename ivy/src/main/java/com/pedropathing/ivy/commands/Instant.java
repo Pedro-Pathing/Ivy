8c0c53464b01d0aca497630d1efff1b99ce4ee99
Kabir <kabirgoyal@icloud.com>
2025-12-30 23:23:13 -0500

    Instant impl

--- ivy/src/main/java/com/pedropathing/ivy/commands/Instant.java
+++ ivy/src/main/java/com/pedropathing/ivy/commands/Instant.java
@@ -1,21 +1,23 @@
 package com.pedropathing.ivy.commands;
 
+import com.pedropathing.ivy.CommandBuilder;
+
 /**
  * A command that runs a given runnable instantly and then finishes.
- * 
+ *
  * @version 1.0
  * @author Baron Henderson
  * @author Kabir Goyal
  */
-public class Instant extends CommandClass {
+public class Instant extends CommandBuilder {
     /**
      * Constructs a new Instant command that runs the given runnable
      * instantly and then finishes.
-     * 
+     *
      * @param r the runnable (void function with no parameters) to execute
      */
     public Instant(Runnable r) {
-        super.setStart(r);
-        super.setDone(() -> true);
+        setStart(r);
+        setDone(() -> true);
     }
 }
